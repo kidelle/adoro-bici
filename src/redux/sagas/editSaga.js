d101575fa7ad94c84e0bddb1d3241b4eda9d9a9b
Danielle Martain <dmartain@gmail.com>
2019-08-17 19:33:11 -0500

    updated some sweet alerts on the delete and edit buttons

--- src/redux/sagas/editSaga.js
+++ src/redux/sagas/editSaga.js
@@ -1,5 +1,6 @@
 import Axios from 'axios';
 import { takeEvery, put } from 'redux-saga/effects';
+import Swal from 'sweetalert2';
 
 
 function* editSaga() {
@@ -9,6 +10,11 @@ function* editReservation(action) {
     try {
         yield Axios.put(`/bikes/${action.payload.id}`, action.payload);
         yield put({ type: 'FETCH_RENTALS' });
+        Swal.fire(
+            'Success!',
+            'Your reservation has been updated!',
+            'success'
+        )
     } catch (error) {
         console.log('Error editing reservation.', error);
         alert('Could not EDIT reservation at this time. Try again later.');
--- src/redux/sagas/removerSaga.js
+++ src/redux/sagas/removerSaga.js
@@ -1,5 +1,6 @@
 import Axios from 'axios';
 import { takeEvery, put } from 'redux-saga/effects';
+import Swal from 'sweetalert2';
 
 
 function* removerSaga() {
@@ -9,6 +10,23 @@ function* removeReservation(action) {
     try {
        yield Axios.delete(`/bikes/${action.payload.id}`);
         yield put({ type: 'FETCH_RENTALS' });
+        Swal.fire({
+            title: 'Are you sure you want to delete your reservation?',
+            text: "You won't be able to undo this!",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.value) {
+                Swal.fire(
+                    'Deleted!',
+                    'Your file has been deleted.',
+                    'success'
+                )
+            }
+        })
     } catch (error) {
         console.log('Error delting reservation.', error);
         alert('Could not delete reservation at this time. Try again later.');
